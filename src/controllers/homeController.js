b4aeef16a41d0af6de7decaeb1838e165a54e512
cuongng <cuongnv.1512@gmail.com>
2022-08-24 07:51:11 +0700

    update controller function

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,20 +1,43 @@
 require('dotenv').config();
+import request from "request";
 
 let getHomepage = (req, res) => {
    return res.render("homePage.ejs")
 };
 
 let postWebhook = (req, res) => {
+   // Parse the request body from the POST
    let body = req.body;
 
-   // Send a 200 OK response if this is a page webhook
+   // Check the webhook event is from a Page subscription
+   if (body.object === 'page') {
 
-   if (body.object === "page") {
-      // Returns a '200 OK' response to all requests
-      res.status(200).send("EVENT_RECEIVED");
+      // Iterate over each entry - there may be multiple if batched
+      body.entry.forEach(function (entry) {
+
+         // Gets the body of the webhook event
+         let webhook_event = entry.messaging[0];
+         console.log(webhook_event);
+
+
+         // Get the sender PSID
+         let sender_psid = webhook_event.sender.id;
+         console.log('Sender PSID: ' + sender_psid);
+
+         // Check if the event is a message or postback and
+         // pass the event to the appropriate handler function
+         if (webhook_event.message) {
+            handleMessage(sender_psid, webhook_event.message);
+         } else if (webhook_event.postback) {
+            handlePostback(sender_psid, webhook_event.postback);
+         }
+
+      });
+
+      // Return a '200 OK' response to all events
+      res.status(200).send('EVENT_RECEIVED');
 
    } else {
-      console.log("Error occur!")
       // Return a '404 Not Found' if event is not from a page subscription
       res.sendStatus(404);
    }
@@ -41,8 +64,60 @@ let getWebhook = (req, res) => {
       }
    }
 }
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+   let response;
+
+   // Check if the message contains text
+   if (received_message.text) {
+
+      // Create the payload for a basic text message
+      response = {
+         "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
+   }
+
+   // Sends the response message
+   callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+   // Construct the message body
+   let request_body = {
+      "recipient": {
+         "id": sender_psid
+      },
+      "message": response
+   }
+
+   // Send the HTTP request to the Messenger Platform
+   request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+   }, (err, res, body) => {
+      if (!err) {
+         console.log('message sent!')
+      } else {
+         console.error("Unable to send message:" + err);
+      }
+   });
+}
+
 module.exports = {
    getHomepage: getHomepage,
    postWebhook: postWebhook,
-   getWebhook: getWebhook
+   getWebhook: getWebhook,
+   handleMessage: handleMessage,
+   handlePostback: handlePostback,
+   callSendAPI: callSendAPI,
 }
